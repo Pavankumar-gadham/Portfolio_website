952efd576953878cc16c666a5befcabccc0f2f44
Pavan Kumar <gpavankumar942@gmail.com>
2025-06-14 14:54:16 +0530

    updated some issues

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,7 +9,7 @@ import Typed from 'typed.js';
 export class HomeComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     new Typed('#typed', {
-      strings: ['Python Enthusiast', 'Frontend Developer', 'Full Stack Developer'],
+      strings: ['Python Engineer', 'Frontend Developer', 'Full Stack Developer'],
       typeSpeed: 50,
       backSpeed: 25,
       loop: true
