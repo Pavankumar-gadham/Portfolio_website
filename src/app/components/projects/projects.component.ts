5f103bd5007b73d12a7ed9e33bc36baa7c0b790e
Pavan Kumar <gpavankumar942@gmail.com>
2025-08-14 18:35:55 +0530

    added project

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -41,8 +41,8 @@ export class ProjectsComponent {
       'Collaborated asynchronously with a distributed team using GitHub and Slack to implement CRUD operations and resolve UI bugs.'],
       tech: ['Angular', 'DRF', 'Python'],
       image: 'assets/images/recipe.jpeg',
-      liveLink: '',
-      codeLink: 'https://github.com/Pavankumar-gadham/E-commerce-RecipeHUb'
+      liveLink: 'https://foodie-heaven-black.vercel.app/home',
+      codeLink: 'https://github.com/Pavankumar-gadham/Foodieheaven_backend'
     },
     {
       name: 'Heart Disease Identification',
