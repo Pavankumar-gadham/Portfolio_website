508c551861eee32f74bdbc38794b36e1244d743d
Pavan Kumar <gpavankumar942@gmail.com>
2025-12-23 14:50:43 +0530

    updated details

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -12,12 +12,14 @@ export class ExperienceComponent {
       company:'ADYAHR BUSINESS SERVICES PRIVATE LIMITED',
       period: 'Dec 2023 - Present',
       description: [
-            "•  Built and maintained CI/CD pipelines using GitHub Actions/Jenkins to automate builds, testing, and deployment for backend and frontend services.",
-            "•  Containerized applications using Docker and optimized images, reducing deployment time by 30%.",
-            "•  Managed deployments on Render and cloud-like environments, achieving 99.9% uptime and zero‑downtime rollouts.", 
-            "•  Monitored logs, application performance, and errors using built‑in monitoring tools, improving system reliability by 40%.", 
-            "•  Collaborated with remote and on‑site teams via GitHub, Slack, and Agile boards to resolve 95% of deployment and integration issues.",
-            "•  Automated scheduled tasks and background jobs using Celery/cron to improve operational efficiency."
+            "•  Built and maintained CI/CD pipelines using Jenkins and GitHub Actions, reducing manual deployment effort by ~30–40%.",
+            "•  Automated build, test, and deployment workflows, reducing release time to ~20–30 minutes.",
+            "•  Containerized backend and frontend services using Docker, improving deployment consistency across   environments.", 
+            "•  Supported Kubernetes-based runtime environments, ensuring smooth rollouts and basic scalability.", 
+            "•  Deployed and managed infrastructure on AWS (EC2, VPC, Security Groups) for reliable application delivery.",
+            "•  Implemented monitoring and logging using AWS CloudWatch, reducing issue resolution time by ~25%.",
+            "•  Collaborated with teams to resolve CI/CD and deployment issues, achieving ~95–98% deployment success rate."
+
           ]
       
     },
